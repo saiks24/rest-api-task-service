2e53d17fc875233d16b4756d0a97c2b0f8d74af5
saiks24 <saiks1337@yandex.ru>
2019-03-28 13:19:02 +0300

    Add boostrap and command controller

--- src/App/App.php
+++ src/App/App.php
@@ -1,7 +1,38 @@
 <?php
 namespace Saiks24\App;
 
+use Saiks24\Http\CommandController;
+
 class App
 {
+    private static $instance;
 
+    private function __construct()
+    {
+        $this->bootstrap();
+    }
+
+    public static function make()
+    {
+        if(!empty(static::$instance)) {
+            return static::$instance;
+        }
+        $app = new App();
+        self::$instance = $app;
+        return $app;
+    }
+
+    private function bootstrap()
+    {
+        try {
+            $app = new \Slim\App();
+            $app->post('/api/v1/command/create',CommandController::class.':create');
+            $app->delete('/api/v1/command/delete',CommandController::class.':delete');
+            $app->get('/api/v1/command/info',CommandController::class.':info');
+            $app->run();
+        } catch (\Exception $e) {
+            echo 'Bootstrap exception:'. $e->getMessage() . PHP_EOL;
+            exit(-1);
+        }
+    }
 }
---
+++ src/Http/CommandController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Saiks24\Http;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class CommandController
+{
+
+    public function create(ServerRequestInterface $request, ResponseInterface $response)
+    {
+
+    }
+
+    public function delete(ServerRequestInterface $request, ResponseInterface $response)
+    {
+
+    }
+
+    public function info(ServerRequestInterface $request, ResponseInterface $response)
+    {
+
+    }
+}
