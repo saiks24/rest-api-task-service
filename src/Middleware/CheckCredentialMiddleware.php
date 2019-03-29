ff89173cbf1f529027eb58a01bafb63eb4f882c4
saiks24 <saiks1337@yandex.ru>
2019-03-29 17:29:16 +0300

    Add CheckCredentialTokenMiddleware realisation

--- src/Middleware/CheckCredentialMiddleware.php
+++ src/Middleware/CheckCredentialMiddleware.php
@@ -3,20 +3,33 @@ namespace Saiks24\Middleware;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Http\Response;
+use Slim\Http\StatusCode;
 
 class CheckCredentialMiddleware
 {
     public function __invoke(ServerRequestInterface $request,ResponseInterface $response, $next)
     {
-       // $token = $request->getHeaderLine('Authorization');
-        //$body = $response->getBody();
-        //$body->write($token);
-       // $response = $response->withBody($body);
+        try {
+            $token = $request->getHeaderLine('Authorization');
+            if(empty($token)) {
+                throw new \InvalidArgumentException('Token required in Authorization header');
+            }
+            /** @var \Psr\Http\Message\ResponseInterface $response */
+            $response = $next($request,$response);
 
-        /** @var \Psr\Http\Message\ResponseInterface $response */
-        $response = $next($request,$response);
+            return $response;
+        } catch (\InvalidArgumentException $e) {
+            $response = $response
+              ->withHeader('Content-Type','application/json')
+              ->withHeader('Cache-Control','private, no-cache, max-age=0, must-revalidate')
+              ->withStatus(403);
+            $body = $response->getBody();
+            $body->write(\json_encode(['status'=>'error','message'=>$e->getMessage()]));
+            $response->withBody($body);
+            return $response;
+        }
 
-        return $response;
     }
 
     private function validateToken(string $token)
