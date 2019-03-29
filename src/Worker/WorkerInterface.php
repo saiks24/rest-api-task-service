8624e60c7ccec66a1c5da57f44824ce73320c018
saiks24 <saiks1337@yandex.ru>
2019-03-29 16:01:51 +0300

    Add meta info to composer.json file

--- src/Worker/WorkerInterface.php
+++ src/Worker/WorkerInterface.php
@@ -4,4 +4,18 @@ namespace Saiks24\Worker;
 interface WorkerInterface
 {
 
+    /** Run queue worker
+     * @return mixed
+     */
+    public function run();
+
+    /** Stop queue worker
+     * @return mixed
+     */
+    public function stop();
+
+    /** Get status of worker process
+     * @return array
+     */
+    public function getStatus() : array ;
 }
