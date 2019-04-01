b9083b60f4d69992a452e8e7e8f1fb3da3728347
saiks24 <saiks1337@yandex.ru>
2019-04-01 15:56:34 +0300

    fix typo

--- src/Middleware/RateLimiter.php
+++ src/Middleware/RateLimiter.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Http\Response;
 
-class ReitLimiter
+class RateLimiter
 {
     /** @var \Redis */
     private $redis;
