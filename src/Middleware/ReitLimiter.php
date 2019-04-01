2fd9145b5a19b2fbb388fa6b17277f0701c7f7ce
saiks24 <saiks1337@yandex.ru>
2019-04-01 15:29:01 +0300

    Add logick to ReitLimmiter

--- src/Middleware/ReitLimiter.php
+++ src/Middleware/ReitLimiter.php
@@ -11,11 +11,23 @@ class ReitLimiter
     /** @var \Redis */
     private $redis;
 
-    public function __construct(\Redis $redis)
+    /** int */
+    private $requestLimit;
+
+    public function __construct(\Redis $redis,int $requestLimit)
     {
         $this->redis = $redis;
+        $this->requestLimit = $requestLimit;
     }
 
+
+    /** Check that client don't send requests many than limit
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Slim\Http\Response                      $response
+     * @param                                          $next
+     *
+     * @return ResponseInterface
+     */
     public function __invoke(ServerRequestInterface $request, Response $response,$next)
     {
         $this->redis->connect('0.0.0.0');
@@ -30,22 +42,43 @@ class ReitLimiter
         return $response;
     }
 
+    /**
+     * @param string $responseId
+     *
+     * @return bool
+     */
     private function isOverReit(string $responseId) : bool
     {
-
+        $limit = $this->redis->get('limiter:'.$responseId);
+        return (int)$limit > $this->requestLimit;
     }
 
+    /** Increment count of requests
+     * @param string $responseId
+     */
     private function incrementReitById(string $responseId)
     {
-
+        $this->redis->incr($responseId);
     }
 
-    private function generateIdByRequest(ServerRequestInterface $response) : string
+    /** Generate id for request
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     *
+     * @return string
+     */
+    private function generateIdByRequest(ServerRequestInterface $request) : string
     {
-
+        $token = $request->getHeaderLine('Authorization');
+        $id = $request->getUri() . '::' . $token . '::' . time();
+        return $id;
     }
 
-    private function createBadRequest(ResponseInterface $response)
+    /** Create 400 Response
+     * @param \Psr\Http\Message\ResponseInterface $response
+     *
+     * @return ResponseInterface
+     */
+    private function createBadRequest(ResponseInterface $response) : ResponseInterface
     {
         $badRequestResponse = $response->withStatus(400);
         $body = $badRequestResponse->getBody();
