9ff7594db64e1a3ffd4cdf918400914164e9c0f8
saiks24 <saiks1337@yandex.ru>
2019-04-02 14:13:08 +0300

    Init codecept

---
+++ tests/api/CheckCredentialMiddlewareCest.php
@@ -0,0 +1,13 @@
+<?php 
+
+class CheckCredentialMiddlewareCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ApiTester $I)
+    {
+    }
+}
