8827ded53a00ea91654141b8898cabf2a90441e5
saiks24 <saiks1337@yandex.ru>
2019-04-03 14:59:20 +0300

    Fix test api configuration. Add check credential middleware api test

--- tests/api/CheckCredentialMiddlewareCest.php
+++ tests/api/CheckCredentialMiddlewareCest.php
@@ -6,8 +6,17 @@ class CheckCredentialMiddlewareCest
     {
     }
 
-    // tests
-    public function tryToTest(ApiTester $I)
+    public function tryToTestCredentialMiddleware(ApiTester $I)
     {
+        $I->wantToTest('Что без токена досутпа сервер отдаст 401');
+        $I->sendGET('/api/v1/command/info',['id'=>'c25125bf853ef7cf70ade98c3ed5b4fc']);
+        $I->seeResponseCodeIsClientError();
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+          'status' => 'error'
+        ]);
+        $I->seeResponseContainsJson([
+          'message' => 'Token required in Authorization header'
+        ]);
     }
 }
