6bec3ed12763d733ed9adacf9482e2f6d3e1c0e6
saiks24 <saiks1337@yandex.ru>
2019-04-05 18:55:36 +0300

    Add config to repo

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+return [
+  'redis' => [
+    'host' => '0.0.0.0',
+    'port' => 6379,
+  ],
+  'amqp' => [
+    'host' => '0.0.0.0',
+    'port' => 5672,
+    'vhost' => '/',
+    'login' => 'guest',
+    'password' => 'guest'
+  ],
+  'rateLimit' => 5,
+  'token' => 'kjnxy1fjj1o231t05tes'
+];
